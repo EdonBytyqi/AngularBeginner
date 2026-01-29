90df8126b62a2a53b1783326300276af705e59dd
Edon Bytyqi <edonbytyqi@outlook.com>
2026-01-29 14:29:51 +0100

    angular-ai-tutor project Modul 5

--- angular-ai-tutor/src/app/app.ts
+++ angular-ai-tutor/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,26 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('angular-ai-tutor');
   protected readonly welcomeMessage = signal('Welcome to your Angular AI Tutor!');
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected readonly servings = signal<number>(0);
 
-  onButtonClick() {
-    console.log('Button was clicked!');
+  protected showNextRecipe(): void {
+    this.recipe.set(MOCK_RECIPES[1]);
+    console.log('Current Recipe:', this.recipe().name);
+  }
+
+  protected showPreviousRecipe(): void {
+    this.recipe.set(MOCK_RECIPES[0]);
+    console.log('Current Recipe:', this.recipe().name);
+  }
+
+  protected increment(): void {
+    this.servings.update((val) => val + 1);
+    console.log('Incremented to:', this.servings());
+  }
+
+  protected decrement(): void {
+    this.servings.update((val) => val - 1);
+    console.log('Decremented to:', this.servings());
   }
 }
